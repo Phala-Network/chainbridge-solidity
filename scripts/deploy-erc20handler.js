9f0487a89b68abc8f60d1f77c92e0b5b4789b23f
Wenfeng Wang <kalot.wang@gmail.com>
2022-07-21 18:01:41 +0800

    update erc20handler deployment script

--- scripts/deploy-erc20handler.js
+++ scripts/deploy-erc20handler.js
@@ -1,35 +1,31 @@
-// Usage:
-//    KEY=<key> GASLIMIT=<gas limit> GASPRICE=<gas price> ENDPOINT=<ethereum endpoint> node script/deploy-erc20handler.js
-
-
 require('dotenv').config();
+const ERC20Handler = require('../build/contracts/ERC20Handler.json');
 const ethers = require('ethers');
 
-const ERC20Handler = require('../build/contracts/ERC20Handler.json');
-const BridgeAddress = '0x181Ec55429e6188D8A22aE8e251f7282D952B3cf';
-// const PHAAdderss = '0x512f7a3c14b6ee86c2015bc8ac1fe97e657f75f2';
-// const ResourceId = '0x00000000000000000000000000000063a7e2be78898ba83824b0c0cc8dfb6001';
-
-async function deployERC20Handler(env) {
+async function deployERC20Handler(env, bridge, intialRid, initialToken) {
     const factory = new ethers.ContractFactory(ERC20Handler.abi, ERC20Handler.bytecode, env.wallet);
-    const contract = await factory.deploy(BridgeAddress, [], [], [], { gasPrice: env.gasPrice, gasLimit: env.gasLimit});
+
+    const contract = await factory.deploy(bridge, [intialRid], [initialToken], [], { gasPrice: env.gasPrice, gasLimit: env.gasLimit});
     await contract.deployed();
-    env.erc20Handler = contract.address;
-    console.log("✓ ERC20Handler deployed", contract.address);
+    console.log("✓ ERC20Handler contract deployed: " + contract.address);
 }
 
 async function main() {
     let env = {};
-    env.url = process.env.ENDPOINT || 'https://mainnet.infura.io/v3/6d61e7957c1c489ea8141e947447405b';
+    env.url = process.env.ENDPOINT || 'http://localhost:8545';
     env.privateKey = process.env.KEY;
     env.provider = new ethers.providers.JsonRpcProvider(env.url);
     env.wallet = new ethers.Wallet(env.privateKey, env.provider);
     env.gasLimit = ethers.utils.hexlify(Number(process.env.GASLIMIT));
     env.gasPrice = ethers.utils.hexlify(Number(process.env.GASPRICE));
 
-    await deployERC20Handler(env);
+    const bridge = process.env.BRIDGE;
+    const rid = process.env.RID;
+    const token = process.env.TOKEN;
+
+    await deployERC20Handler(env, bridge, rid, token)
 }
 
 main()
-  .catch(console.error)
-  .finally(() => process.exit());
+    .catch(console.error)
+    .finally(() => process.exit());
